f36aa7059e5cee11e25b8f95178dfc9690a50ffa
Pablo Nicolas Diaz Bilotto <PabloNicolasDiaz@users.noreply.github.com>
2023-07-28 01:50:45 -0300

    initial commit

---
+++ README.md
@@ -0,0 +1,59 @@
+# parcel-resolver-modernizr
+A [Parcel](https://github.com/parcel-bundler/parcel) resolver for custom [Modernizr](https://github.com/Modernizr/Modernizr) builds, migrate/inspired in [parcel-plugin-modernizr](https://github.com/hirasso/parcel-plugin-modernizr/)
+
+## Installation
+
+#### Using NPM
+
+```
+$ npm install parcel-resolver-modernizr -D
+```
+
+## Setup 
+
+1. Create a file `modernizr-config.json` (you can also use `.modernizrrc`) in your project root folder with your desired Modernizr configuration. You can also use [Modernizr](https://modernizr.com/download) to generate a custom configuration for your project.
+
+2. Include "parcel-resolver-modernizr" in your .parcelrc config file, remember to add all the resolvers previously configured
+```
+{
+  "extends": "@parcel/config-default",
+  "resolvers": ["parcel-resolver-modernizr", "..."]
+}
+```
+
+## Usage: Embedded in html, similar to [parcel-plugin-modernizr](https://github.com/hirasso/parcel-plugin-modernizr) usage option 1
+
+./src/index.html:
+
+```html
+<script src="modernizr.js"></script>
+```
+./src/modernizr-config.json: Your modernizr config 
+
+
+```
+$ parcel ./src/index.html
+```
+
+#### Output
+
+1. ./dist/index.html:
+
+	```html
+	<script src="modernizr.contentHash.js"></script>
+	```
+2. ./dist/modernizr.contentHash.js: Your custom modernizr build
+
+## Limitations
+
+1. package.json option is currently unavailiable
+2. resource name 'modernizr.js' is not configurable
+
+## Contributing
+
+Pull requests are welcome!
+
+
+
+
+
---
+++ test/test.js
@@ -0,0 +1,57 @@
+"use strict";
+const proxyquire = require("proxyquire");
+const sinon = require("sinon");
+const assert = require("assert");
+
+let pathStub = {};
+let Modernizr = {};
+const resolver = proxyquire("../lib/resolver-modernizr", {
+    modernizr: Modernizr,
+    path: pathStub,
+});
+
+describe("parcel-resolver-modernizr", function () {
+    afterEach(function () {
+        sinon.restore();
+    });
+
+    const plugin = resolver[Symbol.for("parcel-plugin-config")];
+
+    describe("the resolver configure the plugin correctly ", function () {
+        it("should open the configuration from modernizr files", async function () {
+            let config = { getConfig: function () {} };
+            sinon.stub(config, "getConfig").returns({ contents: "someContent" });
+            const content = await plugin.loadConfig({ config });
+            assert.equal(content, "someContent");
+        });
+    });
+
+    describe("should resolve the correct resource name", function () {
+        const theFilePath = "the/file/path.js";
+        const config = "someConfigValue";
+        const modernizrOutput = "someModernizr";
+
+        beforeEach(function () {
+            sinon.stub(Modernizr, "build").yieldsAsync(modernizrOutput).calledWith(config);
+            sinon.stub(pathStub, "join").returns(theFilePath);
+        });
+
+        it("should resolve the modernizr.js resource", async function () {
+            const resource = {
+                specifier: "modernizr.js",
+                config,
+            };
+            const resolvedResource = await plugin.resolve(resource);
+            assert.deepEqual(resolvedResource, { code: modernizrOutput, filePath: theFilePath });
+        });
+
+        it("should resolve other resources to null", async function () {
+            const resource = {
+                specifier: "someResource.js",
+                config,
+            };
+            const resolvedResource = await plugin.resolve(resource);
+            assert.equal(resolvedResource, null);
+        });
+    });
+});
